9c281196a6c538f64a1309f75ab772d4ecd0201a
Jessie Yang <jessiejxyang@outlook.com>
2018-01-29 11:00:30 -0800

    Comment CameraControl script and add Tank to CameraRig's Targets

--- Assets/Scripts/Camera/CameraControl.cs
+++ Assets/Scripts/Camera/CameraControl.cs
@@ -2,26 +2,29 @@
 
 public class CameraControl : MonoBehaviour
 {
-    public float m_DampTime = 0.2f;                 
-    public float m_ScreenEdgeBuffer = 4f;           
-    public float m_MinSize = 6.5f;                  
-    [HideInInspector] public Transform[] m_Targets; 
+    public float m_DampTime = 0.2f;                 // approx. time we want to take for the camera to move to where it should be
+    public float m_ScreenEdgeBuffer = 4f;           // "padding"
+    public float m_MinSize = 6.5f;                  // min size of camera, not suuuper zoomed in
+    /*[HideInInspector]*/ public Transform[] m_Targets; // array of transforms for the tanks
 
 
     private Camera m_Camera;                        
-    private float m_ZoomSpeed;                      
-    private Vector3 m_MoveVelocity;                 
-    private Vector3 m_DesiredPosition;              
+    private float m_ZoomSpeed;                      // how fast we're zooming; help damp camera
+    private Vector3 m_MoveVelocity;                 // how fast we're moving ; help damp camera
+    private Vector3 m_DesiredPosition;              // where we want camera to be (want avg of position of the two tanks);
+                                                    //    we'll zoom to this position, not original position
 
 
     private void Awake()
     {
-        m_Camera = GetComponentInChildren<Camera>();
+        m_Camera = GetComponentInChildren<Camera>();  // finds first camera out of children
     }
 
 
     private void FixedUpdate()
     {
+        // we use fixedupdate because tanks are moving like physics objects;
+        //     we want to keep things in sync
         Move();
         Zoom();
     }
@@ -29,8 +32,10 @@ public class CameraControl : MonoBehaviour
 
     private void Move()
     {
+        // avg position between tanks
         FindAveragePosition();
-
+        // smoothly transition between current and desired position;
+        //     note: `ref` means "write back to this variable"
         transform.position = Vector3.SmoothDamp(transform.position, m_DesiredPosition, ref m_MoveVelocity, m_DampTime);
     }
 
@@ -40,6 +45,7 @@ public class CameraControl : MonoBehaviour
         Vector3 averagePos = new Vector3();
         int numTargets = 0;
 
+        // counts # of active targets, sum of positions
         for (int i = 0; i < m_Targets.Length; i++)
         {
             if (!m_Targets[i].gameObject.activeSelf)
@@ -52,6 +58,7 @@ public class CameraControl : MonoBehaviour
         if (numTargets > 0)
             averagePos /= numTargets;
 
+        // don't change y-component of the rig, don't want it to leave the ground
         averagePos.y = transform.position.y;
 
         m_DesiredPosition = averagePos;
@@ -71,6 +78,7 @@ public class CameraControl : MonoBehaviour
 
         float size = 0f;
 
+        // looks for the active tank that's furthest away from center
         for (int i = 0; i < m_Targets.Length; i++)
         {
             if (!m_Targets[i].gameObject.activeSelf)
@@ -78,21 +86,24 @@ public class CameraControl : MonoBehaviour
 
             Vector3 targetLocalPos = transform.InverseTransformPoint(m_Targets[i].position);
 
+            // using the local space of camera rig
             Vector3 desiredPosToTarget = targetLocalPos - desiredLocalPos;
 
+            // check if current tank is further from center than previous tanks along y-axis
             size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.y));
 
+            // check if current tank is further from center along x-axis
             size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.x) / m_Camera.aspect);
         }
         
-        size += m_ScreenEdgeBuffer;
-
-        size = Mathf.Max(size, m_MinSize);
-
+        size += m_ScreenEdgeBuffer;         // add "padding" to edges
+        size = Mathf.Max(size, m_MinSize);  // stay at least a min size
         return size;
     }
 
-
+    
+    // usable by a game manager
+    // reset each round, BUT don't want to smoothly move to that position, avoid damping
     public void SetStartPositionAndSize()
     {
         FindAveragePosition();

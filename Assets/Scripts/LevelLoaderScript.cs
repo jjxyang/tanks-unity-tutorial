8e21e4ee3e3cfc374b02a1282f18962c65d216db
Jessie Yang <jessiejxyang@outlook.com>
2018-02-09 12:47:06 -0800

    Add loading bar to Menu scene when user presses Play

---
+++ Assets/Scripts/LevelLoaderScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelLoaderScript : MonoBehaviour {
+    public GameObject loadingBar;
+    public Slider loadingSlider;
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        // operating asynchronously, i.e. in a coroutine
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingBar.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            loadingSlider.value = progress;
+
+            // will wait until next frame to continue
+            yield return null;
+        }
+    }
+}
